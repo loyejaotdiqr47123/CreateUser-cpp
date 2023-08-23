742e88ecb9bd12ccf15613f95f993cff78b4bb21
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2023-08-23 18:42:39 +0800

    Update CreateUser.exe.asm

--- CreateUser.exe.asm
+++ CreateUser.exe.asm
@@ -1,112 +1,88 @@
 section .data
-    successMsg db "用户创建成功！", 0
-    failureMsg db "用户创建失败！", 0
-    netapi32Lib db "netapi32.dll", 0
-    pNetUserAdd db "NetUserAdd", 0
-    pNetLocalGroupAddMembers db "NetLocalGroupAddMembers", 0
-    userName db 256 dup(0)
-    password db 256 dup(0)
-    groupName db "Users", 0
+    user_msg db '用户开通成功！', 0
+    password_msg db '请输入密码：', 0
+    fail_msg db '用户开通失败！', 0
+    user GROUP  db 'users', 0
+    nerr equ 2226 ;Error code for user already exist
 
 section .text
+    global _start
 
-global main
+_start:
+    ; 检查命令行参数
+    cmp dword [esp+4], 3
+    jne invalid_args
 
-extern GetCommandLineA
-extern CommandLineToArgvA
-extern NetUserAdd
-extern NetLocalGroupAddMembers
+    ; 获取用户名和密码
+    mov eax, [esp+8] ; 用户名（argv[1]）
+    mov ecx, [esp+12] ; 密码（argv[2]])
 
-main:
-    ; 获取命令行参数
-    call GetCommandLineA
-    mov esi, eax ; save command line address
-    
-    push 0 ; argument count
-    push esi ; command line
-    call CommandLineToArgvA
-    add esp, 8
-    mov edi, eax ; save argument list pointer
-    
-    ; 参数个数
-    mov ecx, dword [edi]
-    cmp ecx, 3
-    jne end
-    
-    ; 参数1
-    mov eax, dword [edi + 4]
-    mov ebx, userName
-    mov ecx, 256
-    rep movsb
-    
-    ; 参数2
-    mov eax, dword [edi + 8]
-    mov ebx, password
-    mov ecx, 256
-    rep movsb
-    
-    ; 加载 netapi32.dll
-    push netapi32Lib
-    call LoadLibraryA
-    mov ecx, eax ; save library handle
-    
-    ; 获取 NetUserAdd 函数地址
-    push pNetUserAdd
-    push ecx ; library handle
-    call GetProcAddress
-    mov ebx, eax ; save function address
-    
-    ; 获取 NetLocalGroupAddMembers 函数地址
-    push pNetLocalGroupAddMembers
-    push ecx ; library handle
-    call GetProcAddress
-    mov ecx, eax ; save function address
-    
-    ; 创建用户名结构体
-    push 0 ; level
-    push 2 ; V2_USER_INFO
-    push 0 ; reserved
-    push 0 ; SID (we don't need it)
-    push password ; password
-    push userName ; username
-    push 0 ; home directory
-    push 0 ; comment
-    mov esi, esp ; save struct pointer
-    
-    ; 调用 NetUserAdd
-    push 0 ; parm_err
-    call ebx ; function address
-    add esp, 4 * 9
-    
-    cmp eax, 0 ; 检查返回值
-    jne fail ; 跳转到失败处理
-    
-    ; 添加到本地组Users
-    push 0 ; level
-    push esi ; user info (NetUserAdd)
-    push groupName ; local group name
-    push 0 ; Comment
-    mov ebx, esp ; save struct pointer
-    
-    ; 调用 NetLocalGroupAddMembers
-    push 0 ; total entries
-    call ecx ; function address
-    add esp, 4 * 4
-    
-    jmp end
-    
+    ; 开通用户
+    push 0 ; options (reserved)
+    push ecx ; password
+    push eax ; username
+    push 0 ; level (USER_PRIV_USER)
+    push 1 ; new_user
+    push offset user ; local_group
+    push 0 ; flags
+    call NetUserAdd
+    add esp, 28 ; 清理堆栈
+
+    ; 检查开通用户的结果
+    cmp eax, 0
+    je success
+    cmp eax, nerr
+    je already_exist
+    jmp fail
+
+success:
+    ; 输出成功提示
+    mov eax, 4 ; file descriptor: STDOUT
+    mov ebx, 1 ; buffer
+    mov ecx, user_msg ; message
+    mov edx, 13 ; message length
+    int 0x80 ; system call
+
+    ; 退出程序
+    mov eax, 1 ; system call number for exit
+    xor ebx, ebx ; exit status
+    int 0x80 ; system call
+
+already_exist:
+    ; 输出用户已存在提示
+    mov eax, 4 ; file descriptor: STDOUT
+    mov ebx, 1 ; buffer
+    mov ecx, fail_msg ; message
+    mov edx, 13 ; message length
+    int 0x80 ; system call
+
+    ; 退出程序
+    mov eax, 1 ; system call number for exit
+    xor ebx, ebx ; exit status
+    int 0x80 ; system call
+
 fail:
-    ; 用户创建失败提示
-    push failureMsg
-    call MessageBoxA
-    add esp, 8
-    
-    jmp end
-    
-end:
-    ; 用户创建成功提示
-    push successMsg
-    call MessageBoxA
-    add esp, 8
-    
-    ret
+    ; 输出失败提示
+    mov eax, 4 ; file descriptor: STDOUT
+    mov ebx, 1 ; buffer
+    mov ecx, fail_msg ; message
+    mov edx, 13 ; message length
+    int 0x80 ; system call
+
+    ; 退出程序
+    mov eax, 1 ; system call number for exit
+    xor ebx, ebx ; exit status
+    int 0x80 ; system call
+
+invalid_args:
+    ; 输出参数错误提示
+    mov eax, 4 ; file descriptor: STDOUT
+    mov ebx, 1 ; buffer
+    mov ecx, invalid_args_msg ; message
+    mov edx, 14 ; message length
+    int 0x80 ; system call
+
+    ; 退出程序
+    mov eax, 1 ; system call number for exit
+    xor ebx, ebx ; exit status
+    int 0x80 ; system call
