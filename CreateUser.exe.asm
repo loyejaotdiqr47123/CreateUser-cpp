50244af8503bf2d0ee28a3849a0ebb79cf957439
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2023-08-23 18:14:07 +0800

    Update CreateUser.exe.asm

--- CreateUser.exe.asm
+++ CreateUser.exe.asm
@@ -1,73 +1,112 @@
 section .data
-    username db 0
-    password db 0
-
-section .bss
-    user_info resb 660
+    successMsg db "用户创建成功！", 0
+    failureMsg db "用户创建失败！", 0
+    netapi32Lib db "netapi32.dll", 0
+    pNetUserAdd db "NetUserAdd", 0
+    pNetLocalGroupAddMembers db "NetLocalGroupAddMembers", 0
+    userName db 256 dup(0)
+    password db 256 dup(0)
+    groupName db "Users", 0
 
 section .text
-    global main
-    extern NetUserAdd, printf
+
+global main
+
+extern GetCommandLineA
+extern CommandLineToArgvA
+extern NetUserAdd
+extern NetLocalGroupAddMembers
 
 main:
     ; 获取命令行参数
-    mov rsi, [rdx + 8]   ; argv[1] 用户名
-    mov rdi, [rdx + 16]  ; argv[2] 密码
-
-    mov rcx, rsi
-    call strlen
-
-    mov r8, rax          ; 用户名长度
-    mov rsi, rsi          ; 用户名
-
-    mov rcx, rdi
-    call strlen
-
-    mov rdx, rax          ; 密码长度
-    mov rdi, rdi          ; 密码
-
-    ; 创建用户
-    push rbp
-    mov rbp, rsp
-
-    mov r8, 3             ; USER_PRIV_USER
-    mov r9, 1             ; UF_SCRIPT
-
-    lea r10, [user_info]
-    mov rax, NetUserAdd
-    call rax
-
-    pop rbp
-
-    ; 输出结果
-    cmp rax, 0
-    je success
-    jmp failure
-
-success:
-    lea rdi, [success_msg]
-    jmp show_message
-
-failure:
-    lea rdi, [failure_msg]
-
-show_message:
-    lea rsi, [format]
-    call printf
-
+    call GetCommandLineA
+    mov esi, eax ; save command line address
+    
+    push 0 ; argument count
+    push esi ; command line
+    call CommandLineToArgvA
+    add esp, 8
+    mov edi, eax ; save argument list pointer
+    
+    ; 参数个数
+    mov ecx, dword [edi]
+    cmp ecx, 3
+    jne end
+    
+    ; 参数1
+    mov eax, dword [edi + 4]
+    mov ebx, userName
+    mov ecx, 256
+    rep movsb
+    
+    ; 参数2
+    mov eax, dword [edi + 8]
+    mov ebx, password
+    mov ecx, 256
+    rep movsb
+    
+    ; 加载 netapi32.dll
+    push netapi32Lib
+    call LoadLibraryA
+    mov ecx, eax ; save library handle
+    
+    ; 获取 NetUserAdd 函数地址
+    push pNetUserAdd
+    push ecx ; library handle
+    call GetProcAddress
+    mov ebx, eax ; save function address
+    
+    ; 获取 NetLocalGroupAddMembers 函数地址
+    push pNetLocalGroupAddMembers
+    push ecx ; library handle
+    call GetProcAddress
+    mov ecx, eax ; save function address
+    
+    ; 创建用户名结构体
+    push 0 ; level
+    push 2 ; V2_USER_INFO
+    push 0 ; reserved
+    push 0 ; SID (we don't need it)
+    push password ; password
+    push userName ; username
+    push 0 ; home directory
+    push 0 ; comment
+    mov esi, esp ; save struct pointer
+    
+    ; 调用 NetUserAdd
+    push 0 ; parm_err
+    call ebx ; function address
+    add esp, 4 * 9
+    
+    cmp eax, 0 ; 检查返回值
+    jne fail ; 跳转到失败处理
+    
+    ; 添加到本地组Users
+    push 0 ; level
+    push esi ; user info (NetUserAdd)
+    push groupName ; local group name
+    push 0 ; Comment
+    mov ebx, esp ; save struct pointer
+    
+    ; 调用 NetLocalGroupAddMembers
+    push 0 ; total entries
+    call ecx ; function address
+    add esp, 4 * 4
+    
+    jmp end
+    
+fail:
+    ; 用户创建失败提示
+    push failureMsg
+    call MessageBoxA
+    add esp, 8
+    
+    jmp end
+    
+end:
+    ; 用户创建成功提示
+    push successMsg
+    call MessageBoxA
+    add esp, 8
+    
     ret
-
-strlen:
-    xor rax, rax
-.loop:
-    cmp byte [rdi + rax], 0
-    jz .done
-    inc rax
-    jmp .loop
-.done:
-    ret
-
-section .data
-    success_msg db "用户创建成功！", 10, 0
-    failure_msg db "用户创建失败！", 10, 0
-    format db "%s", 10, 0
